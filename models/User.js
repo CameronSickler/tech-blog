eb869a64d033447433d9571c7c239268c802e0af
Cameron <sicklecj@gmail.com>
2022-06-05 12:02:52 -0500

    add user model

--- models/User.js
+++ models/User.js
@@ -2,4 +2,42 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
+class User extends Model { }
+
+User.init(
+    {
+        //id
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        //username
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        //password
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5]
+            }
+        }
+    },
+    {
+        // hook here
+
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+
+
 module.exports = User;
