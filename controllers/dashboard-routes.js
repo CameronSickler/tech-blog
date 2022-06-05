7546eeaa0a8632c968aef73935e26307865defc9
Cameron <sicklecj@gmail.com>
2022-06-05 17:50:04 -0500

    add routers to connect route js files to relevant handlebars tested successfully in browser

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,6 +5,19 @@ router.get('/', (req, res) => {
     res.render('dashboard')
 });
 
+router.get('/edit/:id', (req, res) => {
+
+    res.render('edit-post')
+
+});
+
+router.get('/create/', (req, res) => {
+
+    res.render('create-post')
+
+});
+
+
 
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,5 +37,19 @@ router.get('/', (req, res) => {
 });
 
 
+router.get('/login', (req, res) => {
+
+    res.render('login');
+});
+
+
+router.get('/signup', (req, res) => {
+
+
+    res.render('signup');
+
+});
+
+
 
 module.exports = router;
